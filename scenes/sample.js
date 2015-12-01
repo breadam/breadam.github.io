5299377bf90c580b419c02edbe89e7307b77bb66
breadam <cagatay@breadam.com>
2015-12-01 04:16:21 +0200

    added scene 'scene-manager'

--- scenes/sample.js
+++ scenes/sample.js
@@ -1,34 +1,33 @@
 breadam.scene({
-	
-	constructor:function(world){
-		this.base(world);
-		
-		var hello = world.body({
+
+	init:function(){
+
+		var hello = this.body({
 			looks:'text',
 			physics:'dynamic',
 			text:'Hello World',
 			x:100,
 			y:20,
 			mass:1
-			
+
 		});
 
-		var underCons = world.body({
+		var underCons = this.body({
 			looks:'text',
 			physics:'dynamic',
 			text:'(under construction)',
 			x:200,
 			y:80,
 			mass:1
-			
+
 		});
 	},
-/*	
+/*
 	onUpdate:function(world){
 		this.base(world);
 	},
 */
-/*	
+/*
 	onResize:function(world){
 		this.base(world);
 	},
